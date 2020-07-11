92594a9c3476a8da2f5162f761f10bac8f079735
Your Name <your_address_email@domain.com>
2020-07-11 15:47:40 -0400

    created web and mobile navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Navigation from "./components/layout/Navigation";
-import "./assets/styles/styles.scss";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import "antd/dist/antd.css";
+
+import PageRenderer from "./page_renderer";
+import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+import "./assets/styles/styles.scss";
 
 function App() {
+  const user = {
+    first_name: "William",
+    last_name: "Chandler",
+  };
   return (
     <Router>
       <div className="App">
-        <Navigation />
+        <Navigation user={user} />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Avatar } from "antd";
 
 const nav_links = [
   { title: "Home", path: "/" },
   { title: "Blog", path: "/blog" },
-  { title: "Contact Us", path: "/contact-us" },
+  { title: "Contact Us", path: "/contact" },
   { title: "Signin", path: "/signin" },
 ];
 
-const Navigation = () => {
+const Navigation = ({ user }) => {
+  const [menuActive, setMenuActive] = useState(false);
   return (
     <nav className="site-navigation">
-      <span>My React Blog</span>
-      <ul>
-        {nav_links.map((link, index) => {
-          return <Link to={link.path}>{link.title}</Link>;
-        })}
-      </ul>
+      <span className="menu-title">My React Blog</span>
+      <div className={`menu-content-container ${menuActive ? "active" : ""}`}>
+        <ul>
+          {nav_links.map((link, index) => {
+            return (
+              <Link to={link.path} className="nav_link" key={index}>
+                {link.title}
+              </Link>
+            );
+          })}
+        </ul>
+        <span className="menu-avatar-container">
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            size={36}
+          />
+          <span className="menu-avatar-name">
+            {user.first_name} {user.last_name}
+          </span>
+        </span>
+      </div>
+      <i
+        className={`ionicons icon ${
+          !menuActive ? "ion-ios-menu" : "ion-ios-close"
+        }`}
+        onClick={() => setMenuActive(!menuActive)}
+      ></i>
     </nav>
   );
 };
